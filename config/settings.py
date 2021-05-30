6498372837ad4a0f802db415371126b6cb69af26
Macachor, Jerald Louie (Contractor) <JeraldLouie_Macachor@comcast.com>
2021-05-30 11:29:34 -0700

    gunicorn added

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1i@vz(=0_)(&_t6!_yy1zk&0_$27!hx-bh4t4$n24v%maawjxa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["serene-lake-94502.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
